2f3b56c8a3405a7e0ecc8f49a783972785df5516
austin-casey <141893333+austin-casey@users.noreply.github.com>
2026-02-16 15:35:43 -0900

    Completed exercise 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const arr = string.split('');
+    const arrReversed = arr.reverse();
+    const strReversed = arrReversed.join('');
+    return strReversed;
 };
+
+console.log(reverseString('hello there'))
 
 // Do not edit below this line
 module.exports = reverseString;
