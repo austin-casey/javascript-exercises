7f05e3c74162c171c2c06eeaadc96c674bbd43e1
austin-casey <141893333+austin-casey@users.noreply.github.com>
2026-02-16 15:17:42 -0900

    Completed exercise 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
 
-};
+function repeatString(string, num) {
+    let newString = string;
+    if (num < 0) {
+        return 'ERROR'
+    } else if (num == 0) {
+        return '';
+    }
+    for (i = 1; i < num; i++) {
+        newString += string;
+    }
+return newString;
+}
 
+console.log(repeatString('hey', 0))
 // Do not edit below this line
 module.exports = repeatString;
