801f1ea746b8675d1f2664ba4baf52eaabdd71e9
austin-casey <141893333+austin-casey@users.noreply.github.com>
2026-02-18 19:08:33 -0900

    Completed exercise 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
-};
+// const removeFromArray = function(arr, ...toRemove) {
+//     for (number of arr) {
+//         if (toRemove.includes(number)) {
+//             arr.splice(number, 1);
+//         }
+//     }
+//     return arr;
+// };
 
+
+const removeFromArray = function(arr, ...toRemove) {
+
+    let filteredArr = arr.filter(element => !toRemove.includes(element))
+    return filteredArr; 
+}
+
+
+
+
+console.log(removeFromArray([1, 2, 3, 4, 3], 3));
 // Do not edit below this line
 module.exports = removeFromArray;
