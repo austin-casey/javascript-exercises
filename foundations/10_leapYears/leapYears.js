7a73754e38969efa8eb9ddbd907e777e37e92dfe
austin-casey <141893333+austin-casey@users.noreply.github.com>
2026-02-20 14:27:02 -0900

    Completed exercise 10_leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 400 === 0) {
+        return true;
+    }
+    
+    if (year % 100 === 0) {
+        return false;
+    }
 
-};
+    if (year % 4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
+console.log(leapYears(2000)); // is a leap year: returns true
+console.log(leapYears(1985)); // is not a leap year: returns false
+console.log(leapYears(1900));
 // Do not edit below this line
 module.exports = leapYears;
