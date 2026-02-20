21a0ba1f106a62dccd6d8bc2ad7ae83ea9ccdbbf
austin-casey <141893333+austin-casey@users.noreply.github.com>
2026-02-20 12:53:44 -0900

    Completed exercise 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 ||
+        b < 0 ||
+        typeof a !== 'number' ||
+        typeof b !== 'number' ||
+        !Number.isInteger(a) ||
+        !Number.isInteger(b)) {
+            return 'ERROR';
+    }
+    
+    let sum = 0;
+    let arr = [a, b];
+    let sortedArr = arr.sort((a, b) => a - b);
 
-};
-
+    for (let i = sortedArr[0]; i <= sortedArr[1]; i++) {
+        sum += i;
+    }
+    return sum;
+}
 // Do not edit below this line
 module.exports = sumAll;
+
+
+// Syntax: splice(start_index, deleteCount, item1, item2, ...)
