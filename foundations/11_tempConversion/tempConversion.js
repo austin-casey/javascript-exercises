9bcce4b13646163b9a7de21fafcb3e964a2bf6a6
austin-casey <141893333+austin-casey@users.noreply.github.com>
2026-02-21 12:02:22 -0900

    Completed exercise 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitTemp) {
+  let f2cTemp = (farenheitTemp - 32) * 5/9;
+  let f2cTempRounded = Math.round(f2cTemp * 10) / 10;
+  return f2cTempRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let c2fTemp = ((celsiusTemp * 9/5) + 32);
+  let c2fTempRounded =  Math.round(c2fTemp * 10) / 10;
+  return c2fTempRounded;
 };
+
+console.log(convertToCelsius(6))
+console.log(convertToFahrenheit(1))
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+// (32°F − 32) × 5/9 = 0°C
+
+// (0°C × 9/5) + 32 = 32°F
